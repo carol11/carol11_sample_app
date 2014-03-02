cc01a5381abfa98a37c1f1f6fdf91c14079983a6
carol11 <car.hanna123@gmail.com>
2014-03-01 21:01:33 -0500

    Finish Chapter 8 Exercises

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,14 @@
 include ApplicationHelper
 
+
+def valid_signup(user)
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
