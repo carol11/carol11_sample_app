dc1e86ead78240132df1d6d031ff20e569eeb143
carol11 <car.hanna123@gmail.com>
2014-03-09 15:39:12 -0400

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Carol11SampleApp::Application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  root  'static_pages#home'
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  root  :to => 'static_pages#home'
   match '/signup',    to: 'users#new',            via: 'get'
   match '/signin',    to: 'sessions#new',         via: 'get'
   match '/signout',   to: 'sessions#destroy',     via: 'delete'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-        password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -14,9 +14,12 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -35,7 +38,7 @@ describe User do
     it { should_not be_valid }
   end
 
-   describe "when email is not present" do
+  describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
@@ -45,50 +48,21 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        expect(@user).not_to be_valid
-      end
-    end
-  end
-
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        expect(@user).to be_valid
-      end
-    end
-  end
-
   describe "when email address is already taken" do
-      before do
-        user_with_same_email = @user.dup
-        user_with_same_email.email = @user.email.upcase
-        user_with_same_email.save
-      end
-
-      it { should_not be_valid }
-  end
-
-  describe "when password is not present" do
     before do
-      @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
     end
+    
     it { should_not be_valid }
   end
 
-  describe "when password doesn't match confirmation" do
-    before { @user.password_confirmation = "mismatch" }
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
-  
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -96,27 +70,17 @@ describe User do
 
   describe "return value of authenticate method" do
     before { @user.save }
-    let(:found_user) { User.find_by(email: @user.email) }
+    let(:found_user) { User.find_by_email(@user.email) }
 
     describe "with valid password" do
-      it { should eq found_user.authenticate(@user.password) }
+      it { should == found_user.authenticate(@user.password) }
     end
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-      it { should_not eq user_for_invalid_password }
+      it { should_not == user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
-    end
-  end
-
-  describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-
-    it "should be saved as all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
 
@@ -125,4 +89,42 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
   
+  describe "micropost associations" do
+    
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+        
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eql([newer_micropost, older_micropost])
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+      microposts.each do |micropost|
+        expect do
+          Micropost.find(micropost)
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+    end
+  end
 end
