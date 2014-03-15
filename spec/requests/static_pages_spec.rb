edcbb1549e8a67de32b00550a8a00617c43758b5
carol11 <car.hanna123@gmail.com>
2014-03-15 00:40:20 -0400

    Add User Following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,25 +14,27 @@ describe "Static pages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        31.times { FactoryGirl.create(:micropost, user: user) }
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
 
       it "should render the user's feed" do
-        user.feed[1..28].each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
 
-      it "should have micropost count and pluralize" do
-        page.should have_content('microposts')
-      end
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
 
-      it "should paginate after 31" do
-        page.should have_selector('div.pagination')
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
